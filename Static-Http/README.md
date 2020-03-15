002ec5d799f22290733ed16156c59b8ba2a7b41a
付佳伟 <1518492902@qq.com>
2020-03-15 20:51:56 +0800

    catch

--- Static-Http/README.md
+++ Static-Http/README.md
@@ -1,7 +1,7 @@
 # 缓存控制头
-## Cache-Control
+## Cache-Control--强制缓存
     被用于在http请求和响应中，通过指定指令来实现缓存机制。缓存指令是单向的，这意味着在请求中设置的指令，不一定被包含在响应中。  
-    指令不区分大小写，并且具有可选参数，可以用令牌或者带引号的字符串语法。多个指令以逗号分隔。  
+    指令不区分大小写，并且具有可选参数，可以用令牌或者带引号的字符串语法。多个指令以逗号分隔。兼容老版本Expires  
 - 缓存请求指令  
     客户端可以在HTTP请求中使用的标准 Cache-Control 指令。  
 ```
@@ -26,4 +26,30 @@ Cache-control: proxy-revalidate
 Cache-Control: max-age=<seconds>  
 Cache-control: s-maxage=<seconds>  
 ```
-[详细内容](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Cache-Control)
+[详细内容](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Cache-Control)  
+## Last-Modified--比较缓存  
+    响应首部，其中包含源头服务器认定的资源做出修改的日期及时间。 通常被用作一个验证器来判断接收到的或者存储的资源是否彼此一致。由于精确度比  ETag 要低，所以这是一个备用机制。包含有  If-Modified-Since 或 If-Unmodified-Since 首部的条件请求会使用这个字段。  
+
+[详细内容](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Last-Modified)   
+## If-Modified-Since--比较缓存  
+    条件式请求首部，服务器只在所请求的资源在给定的日期时间之后对内容进行过修改的情况下才会将资源返回，状态码为 200  。如果请求的资源从那时起未经修改，那么返回一个不带有消息主体的  304  响应，而在 Last-Modified 首部中会带有上次修改时间。 不同于  If-Unmodified-Since, If-Modified-Since 只可以用在 GET 或 HEAD 请求中。  
+    当与 If-None-Match 一同出现时，它（If-Modified-Since）会被忽略掉，除非服务器不支持 If-None-Match。  
+[详细内容](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/If-Modified-Since)   
+## ETag--比较缓存  
+    ETagHTTP响应头是资源的特定版本的标识符。这可以让缓存更高效，并节省带宽，因为如果内容没有改变，Web服务器不需要发送完整的响应。而如果内容发生了变化，使用ETag有助于防止资源的同时更新相互覆盖（“空中碰撞”）。  
+    如果给定URL中的资源更改，则一定要生成新的Etag值。 因此Etags类似于指纹，也可能被某些服务器用于跟踪。 比较etags能快速确定此资源是否变化，但也可能被跟踪服务器永久存留。  
+### 语法
+```
+ETag: W/"<etag_value>"   
+ETag: "<etag_value>"   
+```
+- W/ 可选  
+'W/'(大小写敏感) 表示使用弱验证器。 弱验证器很容易生成，但不利于比较。 强验证器是比较的理想选择，但很难有效地生成。 相同资源的两个弱Etag值可能语义等同，但不是每个字节都相同。  
+- "<etag_value>"  
+实体标签唯一地表示所请求的资源。 它们是位于双引号之间的ASCII字符串（如“675af34563dc-tr34”）。 没有明确指定生成ETag值的方法。 通常，使用内容的散列，最后修改时间戳的哈希值，或简单地使用版本号。 例如，MDN使用wiki内容的十六进制数字的哈希值。  
+### 避免“空中碰撞”
+提交时根据请求所包含的If-Match来检查是否为最新版本。如果哈希值不匹配，则意味着文档已经被编辑，抛出412前提条件失败错误。  
+### 缓存未更改的资源
+ETag头的另一个典型用例是缓存未更改的资源。 如果用户再次访问给定的URL（设有ETag字段），显示资源过期了且不可用，客户端就发送值为ETag的If-None-Match header字段：  
+服务器将客户端的ETag（作为If-None-Match字段的值一起发送）与其当前版本的资源的ETag进行比较，如果两个值匹配（即资源未更改），服务器将返回不带任何内容的304未修改状态，告诉客户端缓存版本可用（新鲜）。  
+[详细内容](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/ETag)    
--- Static-Http/server.js
+++ Static-Http/server.js
@@ -4,6 +4,7 @@ const url = require('url')
 const chalk = require('chalk')
 const path = require('path')
 const ejs = require('ejs')
+const crypto = require('crypto')
 const mime = require('mime')
 const template = fs.readFileSync(path.resolve(__dirname, 'template.html'), 'utf8')
 class Server {
@@ -29,22 +30,64 @@ class Server {
                 res.setHeader('Content-Type', "text/html;charset=utf8")
                 res.end(str)
             } else {
-                this.sendFile(req, res, absPath)
+                this.sendFile(req, res, stat, absPath)
             }
         } catch (e) {
-            console.log(e)
+            console.error(e)
             this.sendError(req, res, absPath)
         }
     }
 
-    sendFile(req, res, absPath) {
+    sendFile(req, res, stat, absPath) {
         res.setHeader('Content-Type', `${mime.getType(absPath)};charset=utf8`)
+        if(this.readCatch(req, res, stat)) {
+            res.statusCode = 304;
+            return res.end();
+        }
+
         fs.createReadStream(absPath).pipe(res)
+        // const rs = fs.createReadStream(absPath)
+        // const md5 = crypto.createHash('md5')
+        // const arr = []
+        // rs.on('data', (chunk)=>{
+        //     md5.update(chunk)
+        //     arr.push(chunk)
+        // })
+        // rs.on('end', ()=>{
+        //     const serverTag = md5.digest('base64')
+        //     const clentTag = req.headers['if-none-match']
+        //     if(serverTag === clentTag){
+        //         res.statusCode = 304;
+        //         res.end()
+        //         return;
+        //     }
+        //     res.setHeader('ETag', serverTag)
+        //     res.end(Buffer.concat(arr))
+        // })
     }
 
     sendError(req, res) {
         res.statusCode = 404
         res.end('Not Found')
+    }
+
+    readCatch(req, res, stat) {
+        const modifiedSince = req.headers['if-modified-since']
+        const lastModified = stat.ctime.toGMTString()
+        const noneMatch = req.headers['if-none-match']
+        const etag = stat.size+''
+
+        res.setHeader('Catch-Control', 'max-age=5')
+        res.setHeader('Last-Modified', lastModified)
+        res.setHeader('ETag', etag)
+
+        if(lastModified !== modifiedSince) {
+            return false
+        }
+        if(noneMatch !== etag) {
+            return false
+        }
+        return true
     }
 
     start(){
@@ -56,3 +99,6 @@ class Server {
     }
 }
 module.exports = Server
+
+// tips
+// node使用http的请求头取值时要全部小写，当设置请求头时则要大写首字母 
